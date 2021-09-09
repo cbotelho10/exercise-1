b6b2647a349fc5a19cd5c692e6db9b180c042ea3
chris botelho <chris.botelho29@gmail.com>
2021-09-09 14:17:37 -0400

    Made the button clickable

--- exercise-1/ViewController.swift
+++ exercise-1/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
     }
 
-
+    @IBAction func showMessage(_ sender: UIButton) {
+        print("the button has been pressed")
+    }
+    
 }
 
